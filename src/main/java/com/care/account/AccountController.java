7ebea6784fab9c28080dbf85064c218563467e63
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-07-02 05:31:19 +0900

    !

--- src/main/java/com/care/account/AccountController.java
+++ src/main/java/com/care/account/AccountController.java
@@ -22,27 +22,28 @@ public class AccountController {
     //회원가입 폼 검사
     private final SignUpFormValidator signUpFormValidator;
     private final AccountService accountService;
+    private final AccountRepository accountRepository;
 
 
     //↓ "" 객체에 대해 검사를 담당할 Validator를 등록하는 함수
     @InitBinder("signUpForm")
-    public void initBinder(WebDataBinder webDataBinder){
+    public void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(signUpFormValidator);
     }
 
     //"회원가입 폼을 보여주는 부분"
     @GetMapping("/sign-up")
-    public String singUpForm(Model model){
+    public String singUpForm(Model model) {
         //HTMl(jsp)에서 사용할 수 있게 데이터를 넘겨줌↓
-        model.addAttribute("signUpForm",new SignUpForm());
+        model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
     }
 
     //"회원가입을 처리하는 부분" = 사용자가 폼에 작성한 정보를 제출했을 때 처리하는 함수
     @PostMapping("/sign-up")
-    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
         //오류가 발생하면 다시 회원가입 폼으로
-        if (errors.hasErrors()){
+        if (errors.hasErrors()) {
             return "account/sign-up";
         }
 
@@ -52,5 +53,18 @@ public class AccountController {
         return "redirect:/";
     }
 
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(String token, String email, Model model) {
+        Account account = accountRepository.findByEmail(email);
+        if (account == null) {
+            model.addAttribute("error", "wrong.email");
+            return "account/checkedEmail";
+        }
+
+        if(!account.getEmailCheckToken().equals(token)){
+            model.addAttribute("error", "wrong.token");
+            return "account/checkedEmail";
+        }
+    }
 
 }
